4621626ddf19f7de9f664793e7d31c42dc307980
jianwenlong <jian-wenlong@rui-hai.com>
2024-12-04 16:21:00 +0800

    添加项目文件。

---
+++ LargeImageTest/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace LargeImageTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            RenderOptions.ProcessRenderMode = RenderMode.Default;
+        }
+    }
+
+}
